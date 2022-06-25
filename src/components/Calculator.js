407ff75ffd3ba7cd3ad3f3e54add6d03f92193f1
rbeckhorn <rbeckhorn@rsilogistics.com>
2022-06-25 11:24:22 -0400

    #US2 Updates Number Clicks
    Splits Number and Decimal Functions. Changes button input from string value to numeric value

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,41 +1,54 @@
 import React, { useState } from 'react';
 function Calculator() {
   // Declare a new state variable for display
+  // display can potentially show Input2 or Input2 values
    const [display, setDisplay] = useState(0);
+   // Input 1 will handle first numeric value added before operator is clicked
+
    const [input1Value, setInput1Value] = useState("0");
    // Soon: add state for Input1, Operator1, Input2 and Operator2
    //const [input1Value, setInput1Value] = useState(0);
 
   return ( 
       <div>
-        <input id="display" type='text' value={display}></input>
+        <input id="display" type='text' value={display} readOnly></input>
         <div>
-        <span class='btn' onClick={() => handleButtonClick("9")}>9</span>
-        <span class='btn' onClick={() => handleButtonClick("8")}>8</span>
-        <span class='btn' onClick={() => handleButtonClick("7")}>7</span>
+        <span className='btn' onClick={() => handleNumberClick(9)}>9</span>
+        <span className='btn' onClick={() => handleNumberClick(8)}>8</span>
+        <span className='btn' onClick={() => handleNumberClick(7)}>7</span>
         </div>
         <div>
-        <span class='btn' onClick={() => handleButtonClick("6")}>6</span>
-        <span class='btn' onClick={() => handleButtonClick("5")}>5</span>
-        <span class='btn' onClick={() => handleButtonClick("4")}>4</span>
+        <span className='btn' onClick={() => handleNumberClick(6)}>6</span>
+        <span className='btn' onClick={() => handleNumberClick(5)}>5</span>
+        <span className='btn' onClick={() => handleNumberClick(4)}>4</span>
         </div>
         <div>
         <div>
-        <span class='btn' onClick={() => handleButtonClick("3")}>3</span>
-        <span class='btn' onClick={() => handleButtonClick("2")}>2</span>
-        <span class='btn' onClick={() => handleButtonClick("1")}>1</span>
+        <span className='btn' onClick={() => handleNumberClick(3)}>3</span>
+        <span className='btn' onClick={() => handleNumberClick(2)}>2</span>
+        <span className='btn' onClick={() => handleNumberClick(1)}>1</span>
         </div></div>
         <div>
-        <span class='btn' onClick={() => handleButtonClick("0")}>0</span>
-        <span class='btn' onClick={() => handleButtonClick(".")}>.</span>
+        <span className='btn' onClick={() => handleNumberClick(0)}>0</span>
+        <span className='btn' onClick={() => handleDecimalClick()}>.</span>
         </div>
       </div>
     );
 
-    function handleButtonClick(num) {
-      var retNum = display + num
-      //setInput1Value(retNum);
+    function handleNumberClick(num) {
+
+      // Appends digits to number value
+      // Might of handled differently if calculator had parentheses for order of operations
+      const retNum = display * 10 + num
+
+      setInput1Value(retNum);
       setDisplay(retNum)
+    }
+
+    function handleDecimalClick(num) {
+
+      setDisplay(display + ".")
+      //Handle non 2 Decimals
     }
   }
   export default Calculator;
