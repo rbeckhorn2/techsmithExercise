98d59adf3556764a26955558b8b95747bcc5c2ec
rbeckhorn <rbeckhorn@rsilogistics.com>
2022-06-25 13:38:13 -0400

    Fini!

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -7,8 +7,8 @@ function Calculator() {
 
    const [input1Value, setInput1Value] = useState(0);
    const [input2Value, setInput2Value] = useState(0);
-   const [operator1Value, setOperator1Value] = useState("");
-   const [operator2Value, setOperator2Value] = useState("");
+   const [operatorValue, setOperatorValue] = useState("");
+
 
    // Soon: add state for Input1, Operator1, Input2 and Operator2
    //const [input1Value, setInput1Value] = useState(0);
@@ -17,32 +17,24 @@ function Calculator() {
       <div>
         <input id="display" type='text' value={display} readOnly></input>
           <div>
-            <div>
-            <span className='btn' onClick={() => handleNumberClick(9)}>9</span>
-            <span className='btn' onClick={() => handleNumberClick(8)}>8</span>
-            <span className='btn' onClick={() => handleNumberClick(7)}>7</span>
-            <span className='btn' onClick={() => handleOperatorClick("+")}>+</span>
-            </div>
-            <div>
-            <span className='btn' onClick={() => handleNumberClick(6)}>6</span>
-            <span className='btn' onClick={() => handleNumberClick(5)}>5</span>
-            <span className='btn' onClick={() => handleNumberClick(4)}>4</span>
-            <span className='btn' onClick={() => handleOperatorClick("-")}>-</span>
-            </div>
-            <div>
-            <span className='btn' onClick={() => handleNumberClick(3)}>3</span>
-            <span className='btn' onClick={() => handleNumberClick(2)}>2</span>
-            <span className='btn' onClick={() => handleNumberClick(1)}>1</span>
-            <span className='btn' onClick={() => handleOperatorClick("*")}>*</span>
-            </div>
-            <div>
-            <span className='btn' onClick={() => handleNumberClick(0)}>0</span>
-            <span className='btn' onClick={() => handleDecimalClick()}>.</span>
-            <span className='btn' onClick={() => handleEqualSignClick()}>=</span>
-            <span className='btn' onClick={() => handleOperatorClick("/")}>/</span>
-            </div>
-            <div>
-            <span className='btn' onClick={() => handleClearClick()}>AC</span>
+            <div className="grid-container">
+            <div className="grid-item" onClick={() => handleNumberClick(9)}>9</div>
+            <div className="grid-item" onClick={() => handleNumberClick(8)}>8</div>
+            <div className="grid-item" onClick={() => handleNumberClick(7)}>7</div>
+            <div className="grid-item" onClick={() => handleOperatorClick("+")}>+</div>
+            <div className="grid-item" onClick={() => handleNumberClick(6)}>6</div>
+            <div className="grid-item" onClick={() => handleNumberClick(5)}>5</div>
+            <div className="grid-item" onClick={() => handleNumberClick(4)}>4</div>
+            <div className="grid-item" onClick={() => handleOperatorClick("-")}>-</div>
+            <div className="grid-item" onClick={() => handleNumberClick(3)}>3</div>
+            <div className="grid-item" onClick={() => handleNumberClick(2)}>2</div>
+            <div className="grid-item" onClick={() => handleNumberClick(1)}>1</div>
+            <div className="grid-item" onClick={() => handleOperatorClick("*")}>*</div>
+            <div className="grid-item" onClick={() => handleNumberClick(0)}>0</div>
+            <div className="grid-item" onClick={() => handleDecimalClick()}>.</div>
+            <div className="grid-item" onClick={() => handleEqualSignClick()}>=</div>
+            <div className="grid-item" onClick={() => handleOperatorClick("/")}>/</div>
+            <div className="item1" onClick={() => handleClearClick()}>AC</div>
             </div>
           </div>
       </div>
@@ -54,7 +46,7 @@ function Calculator() {
       // Appends digits to number value
       // Might of handled differently if calculator had parentheses for order of operations
      
-      if (operator1Value === "") {
+      if (operatorValue === "") {
         const retNum = input1Value * 10 + num
         setInput1Value(retNum);
         setDisplay(retNum)
@@ -64,8 +56,6 @@ function Calculator() {
         setInput2Value(retNum);
         setDisplay(retNum)
       }
-
-     
     }
 
     function handleDecimalClick(num) {
@@ -76,23 +66,25 @@ function Calculator() {
 
     function handleOperatorClick(op) {
       //alert("Addition");
-      if (operator1Value === "") 
-        setOperator1Value(op);
+      if (operatorValue === "") 
+        setOperatorValue(op);
     }
     
     function handleClearClick() {
       setInput1Value(0);
       setInput2Value(0);
-      setOperator1Value("");
+      setOperatorValue("");
       setDisplay(0);
     }
 
    function  handleEqualSignClick() {
-      alert("Equals");
-      if (operator1Value !== "") {
-        alert("ret1");
-        const retNum = eval(input1Value + operator1Value + input2Value);
-        alert("ret2");
+      if (operatorValue !== "") {
+        const retNum = eval(input1Value + operatorValue + input2Value);
+        //reset Inputs for further calcs
+        setInput1Value(retNum);
+        setInput2Value(0);
+        setOperatorValue("");
+        // return display
         setDisplay(retNum);
      }
    }
